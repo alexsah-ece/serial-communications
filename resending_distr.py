ad48d135e6c1cbef62eb04ca193d445df609f7d3
Alexandros Sahinis <alexsah97@gmail.com>
2018-12-04 17:52:45 +0200

    Add BER calculation

--- resending_distr.py
+++ resending_distr.py
@@ -5,26 +5,41 @@ def make_dict():
     with open("../Data/log_ARQPackets.csv", mode='r') as csv_file:
         csv_reader = csv.DictReader(csv_file, fieldnames=["Packet","Response time","Times requested","Time elapsed"])
         next(csv_reader, None)
-        resending = {}
+        transmissions = {}
         for row in csv_reader:
-            if row["Times requested"] in resending:
-                resending[row["Times requested"]] += 1
+            if row["Times requested"] in transmissions:
+                transmissions[row["Times requested"]] += 1
             else:
-                resending[row["Times requested"]] = 1
-    return resending
+                transmissions[row["Times requested"]] = 1
+    return transmissions
 
-def write_csv(resending):
-    with open("../Data/resending_distr.csv", mode='w+') as csv_file:
+def write_csv(transmissions):
+    with open("../Data/transmissions_distr.csv", mode='w+') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=["Times requested","Frequency"])
         csv_writer.writeheader()
-        for key, value in resending.items():
+        for key, value in transmissions.items():
             print(key, value)
             rowdict = {}
             rowdict["Times requested"] = key
             rowdict["Frequency"] = value
             csv_writer.writerow(rowdict)
 
+def calculate_BER(transmissions):
+    total_transmissions = 0
+    successful_packets = 0
+    for key, value in transmissions.items():
+        total_transmissions += float(key) * float(value)
+        successful_packets += value
+    P = successful_packets / total_transmissions
+    print("P: %f" %(P))
+    ber = 1 - P**(1/float(16*8))
+    print("BER: %f" %(ber))
+    #Pe = 1 - (1-Pp)**(1/)
+
+
+
 if __name__ == "__main__":
     
     dict = make_dict()
     write_csv(dict)
+    calculate_BER(dict)
